dec38f2cc2e86418a70085884915157152546a67
Kelvin Cj <ckelvin196@gmail.com>
2024-01-18 21:40:15 +0100

    added the drawer for both menu and cart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,16 @@
-import { CiUser } from "react-icons/ci";
+import { CiUser,CiSearch,CiHeart } from "react-icons/ci";
 import { IoBagHandleOutline } from "react-icons/io5";
 import { RiMenu2Fill } from "react-icons/ri";
+import { Tooltip } from "@material-tailwind/react";
+import React from "react";
+import {
+  Drawer,
+  Button,
+  Typography,
+  Input,
+  Textarea,
+  IconButton,
+} from "@material-tailwind/react";
 import '../App.css'
 function About(){  
   return(
@@ -9,8 +19,8 @@ function About(){
         <li className="expand">Home</li>
         <li className="expand">Sale</li>
         <li className="expand">New</li>
-        <li className="expand">Contact us</li>
         <li className="expand">New collection</li>
+        <li className="expand">Contact us</li>
         <li className="expand">About us</li>
       </ul>
     </div>
@@ -32,14 +42,121 @@ function Logo(){
 function Menu() {
   return (
 <div className="flex gap-3 items-center">
+<Tooltip content="Wish list">
       <CiUser size={28} className="cursor-pointer"/>
-      <IoBagHandleOutline size={28} className="cursor-pointer" />
-      <RiMenu2Fill size={28} className="cursor-pointer block lg:hidden"/>
+      </Tooltip>
+      <DrawerCart/>
+      {/* <IoBagHandleOutline size={28} className="cursor-pointer" /> */}
+      <Tooltip content="Wish list">
+      <CiHeart size={28} className="cursor-pointer" />
+    </Tooltip>
+      <DrawerMenu/>
+      <CiSearch size={28} className="cursor-pointer hidden md:block"/>
       </div>
   );
 }
 
+function DrawerMenu() {
+  const [open, setOpen] = React.useState(false);
+ 
+  const openDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
+ 
+  return (
+    <React.Fragment>
+      <RiMenu2Fill onClick={openDrawer} size={28} className="cursor-pointer block lg:hidden"/>
+      <Drawer placement="right" open={open} onClose={closeDrawer} className="p-4">
+        <div className="mb-6 flex items-center justify-between">
+          <Typography variant="h5" color="blue-gray">
+           Honorine Menu
+          </Typography>
+          <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </IconButton>
+        </div>
+        <Typography color="gray" className="mb-8 pr-4 font-normal">
+          Material Tailwind features multiple React and HTML components, all
+          written with Tailwind CSS classes and Material Design guidelines.
+        </Typography>
+        <div className="flex gap-2">
+          <Button size="sm" variant="outlined">
+            Documentation
+          </Button>
+          <Button size="sm">Get Started</Button>
+        </div>
+        <form className="flex flex-col gap-6 p-4 pt-9">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Send us a Message
+          </Typography>
+          <Input type="email" label="Email" />
+          <Input label="Subject" />
+          <Textarea rows={6} label="Message" />
+          <Button>Send Message</Button>
+        </form>
 
+      </Drawer>
+    </React.Fragment>
+  );
+}
+
+function DrawerCart() {
+  const [open, setOpen] = React.useState(false);
+ 
+  const openDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
+ 
+  return (
+    <React.Fragment>
+      <IoBagHandleOutline onClick={openDrawer} size={28} className="cursor-pointer" />
+      <Drawer placement="right" open={open} onClose={closeDrawer} className="p-4">
+        <div className="mb-6 flex items-center justify-between">
+          <Typography variant="h5" color="blue-gray">
+           Honorine Cart
+          </Typography>
+          <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </IconButton>
+        </div>
+        <Typography color="gray" className="mb-8 pr-4 font-normal">
+          Material Tailwind features multiple React and HTML components, all
+          written with Tailwind CSS classes and Material Design guidelines.
+        </Typography>
+        <div className="flex gap-2">
+          <Button size="sm" variant="outlined">
+            Documentation
+          </Button>
+          <Button size="sm">Get Started</Button>
+        </div>
+      </Drawer>
+    </React.Fragment>
+  );
+}
 
 function NavBar() {
  
